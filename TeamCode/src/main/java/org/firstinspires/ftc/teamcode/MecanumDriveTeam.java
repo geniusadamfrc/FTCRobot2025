ce1b81d9dc8119214a06668e7d73e9255f836d95
Adam Malacina <geniusadamfrc@gmail.com>
2025-08-29 13:11:56 -0500

    Inclusion of RoadRunner.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTeam.java
@@ -0,0 +1,177 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "MecanumDriveTeam")
+public class MecanumDriveTeam extends OpMode {
+    double oldTime = 0;
+
+    /**
+     * This function is executed when this OpMode is selected from the Driver Station.
+     */
+    @Override
+    public void init(){
+
+        Robot.init(hardwareMap, telemetry);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        Robot.drivetrain.playOnce();
+    }
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        //Grabber = hardwareMap.get(Servo.class, "Grabber");
+        // Put loop blocks here.
+        telemetry.update();
+        telemetry.addData("Left Stick", gamepad1.left_stick_y);
+        //telemetry.addData("Claw", Grabber.getPosition());
+        double forward = -gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+        double strafe = gamepad1.left_stick_x;
+        Robot.drivetrain.manualDrive(forward, turn, strafe);
+        Robot.drivetrain.loop();
+
+        if (gamepad1.a){
+            Robot.drivetrain.resetPosition();
+        }
+        if (gamepad1.b){
+            Robot.drivetrain.recalibrateIMU();
+        }
+    }
+    public void doFrequency(){
+        /*
+            This code prints the loop frequency of the REV Control Hub. This frequency is effected
+            by I²C reads/writes. So it's good to keep an eye on. This code calculates the amount
+            of time each cycle takes and finds the frequency (number of updates per second) from
+            that cycle time.
+             */
+        double newTime = getRuntime();
+        double loopTime = newTime-oldTime;
+        double frequency = 1/loopTime;
+        oldTime = newTime;
+        telemetry.addData("REV Hub Frequency: ", frequency); //prints the control system refresh rate
+    }
+
+
+
+    public void sampleDrive(){
+        /*
+        public void loop() {
+        if(gamepad1.back){
+            imu.resetYaw();
+            return;
+        }
+        if(gamepad2.back){
+            elevator.resetEncoders();
+            elevator.raiseElevatorUnrestricted(gamepad2.left_stick_y);
+
+        return;
+        }
+
+
+        handleGamePad1LeftBumper();
+
+        //elevator Controls
+        if ((gamepad1.y||gamepad2.y) && activeCommand == null){
+             activeCommand  = new MoveToChamberHeight(elevator, elevatorClaw, telemetry);
+             activeCommand.begin();
+        }
+        if ((gamepad1.a||gamepad2.a) && activeCommand == null){
+            activeCommand = new MoveToWallHeight(elevator, elevatorClaw);
+            activeCommand.begin();
+        }
+        if (gamepad1.x||gamepad2.x) elevatorClaw.openClaw();
+        if (gamepad1.b||gamepad2.b) elevatorClaw.closeClaw();
+
+
+
+        if (gamepad1.dpad_up||gamepad2.dpad_up) slideClaw.raiseClaw();
+        if (gamepad1.dpad_down||gamepad2.dpad_down) slideClaw.lowerClaw();
+        if (gamepad1.dpad_left||gamepad2.dpad_left) slideClaw.openClaw();
+        if (gamepad1.dpad_right||gamepad2.dpad_right) slideClaw.closeClaw();
+
+        if (gamepad2.left_bumper && activeCommand == null){
+            activeCommand = new PrepareForIntake(slideClaw);
+            activeCommand.begin();
+        }
+        handleGamePad2RightBumper();
+        linearSlide.manuallyMove(gamepad2.right_trigger-gamepad2.left_trigger);
+
+
+
+
+        //(gamepad2.right_stick_y);
+        elevator.raiseElevator(-gamepad2.left_stick_y);
+        elevator.loop();
+        //for(Command command : commands){
+            //telemetry.addData("Command", "" + command.isFinished());
+            //command.loop();
+        //}
+        if (activeCommand != null) activeCommand.loop();
+        if (activeCommand != null && activeCommand.isFinished()) activeCommand = null;
+        if (activeCommand != null) telemetry.addData("ActiveCommand Running:", activeCommand.isStarted());
+        if (activeCommand != null) telemetry.addData("ActiveCommand Finished:", activeCommand.isFinished());
+
+
+
+
+        // Send telemetry message to signify robot running;
+       telemetry.addData("Slide Position",  ":" + (linearSlide.linearSlide.getCurrentPosition() - linearSlide.startingPosition));
+       telemetry.addData("Elevator Position",  ":" + elevator.getPosition());
+
+       telemetry.addData("Elevator Raw Position",  "" +elevator.getRawPosition());
+       telemetry.addData("ElevatorStatus: " , elevator.getState());
+       telemetry.addData("DrivetrainStatus: " , drivetrain.getState());
+       telemetry.addData("Drivetrain Encoder", drivetrain.getEncoderReading()+"");
+
+       telemetry.addData("Current Time", "" +System.currentTimeMillis() );
+        telemetry.addData("Active Command", activeCommand == null ? "IDLE" : activeCommand.getClass().getName());
+        imu.addTelemetry(telemetry);
+
+    }
+
+    public void handleGamePad1LeftBumper(){
+        if(gamepad1.left_bumper){
+            if (activeCommand == null && leftBumperReleased) {
+                activeCommand = new AttemptChamber(drivetrain, elevator, elevatorClaw, telemetry);;
+                activeCommand.begin();
+                leftBumperReleased = false;
+            }
+        }else {
+            leftBumperReleased = true;
+            if (activeCommand != null && activeCommand instanceof AttemptChamber &&
+                activeCommand.isStarted() && !activeCommand.isFinished())
+                    activeCommand.finish();
+        }
+    }
+    public void handleGamePad2RightBumper(){
+        if(gamepad2.right_bumper){
+            if (activeCommand == null && rightBumperReleased) {
+                activeCommand = new Intake(slideClaw, linearSlide);
+                activeCommand.begin();
+                rightBumperReleased = false;
+            }
+        }else {
+            rightBumperReleased = true;
+            if (activeCommand != null && activeCommand instanceof Intake &&
+                activeCommand.isStarted() && !activeCommand.isFinished())
+                    activeCommand.finish();
+        }
+    }
+         */
+    }
+}
